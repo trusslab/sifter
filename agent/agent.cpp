f6508ffb0d5792671d2c2dbc0f82f24c459c5924
Hsin-Wei Hung <hsinweih@uci.edu>
2020-11-10 00:25:23 -0800

    Add a new map type, ring buffer, for tracing syscall sequences

--- agent/agent.cpp
+++ agent/agent.cpp
@@ -6,10 +6,64 @@
 #include <cstdio>
 #include <fstream>
 #include <vector>
+#include <deque>
+#include <map>
+#include <set>
+#include <bitset>
 #include <sstream>
+#include <thread>
 #include <unistd.h>
 
 using android::base::unique_fd;
+
+
+struct rb_elem {
+    struct bpf_spin_lock lock;
+    uint8_t next;
+    uint16_t id0[128];
+    uint16_t id1[128];
+};
+
+struct u_rb_elem {
+    uint8_t ctr;
+    uint16_t id[256];
+};
+
+struct sifter_rb {
+    int len;
+    std::string name;
+    unique_fd fd;
+    unique_fd ctr_fd;
+    std::vector<u_rb_elem> saved;
+//    std::vector<std::map<std::deque<uint16_t>, std::bitset<65536> > > tbl;
+    std::map<std::deque<uint16_t>, std::bitset<65536> > tbl;
+    sifter_rb(int l, std::string nm, int f, int ctr):
+        len(l), name(nm), fd(android::base::unique_fd(f)),
+        ctr_fd(android::base::unique_fd(ctr)) {
+//        tbl.resize(32768);
+        saved.resize(32768);
+    };
+    void update_tbl(int pid, uint8_t ctr, rb_elem rb, bool missing_events) {
+        bool first_half = (ctr%2 == 0);
+        uint8_t dst_start = first_half? 0 : 128;
+        uint16_t *src_ptr = first_half? rb.id0 : rb.id1;
+        memcpy(&saved[pid].id[dst_start], src_ptr, 256);
+
+        std::deque<uint16_t> seq;
+        uint8_t start = missing_events? dst_start : dst_start-len;
+        for (int i = start; i <= start+len-1; i++)
+            seq.push_back(saved[pid].id[i]);
+        for (int i = start+len; i < start+127; i++) {
+            uint16_t next = saved[pid].id[i];
+            if (tbl.find(seq) != tbl.end())
+                tbl[seq].set(next);
+            else
+                tbl[seq] = std::bitset<65536>(next);
+            seq.pop_front();
+            seq.push_back(next);
+        }
+    };
+};
 
 struct sifter_map {
     int type;
@@ -23,29 +77,79 @@ struct sifter_map {
 };
 
 struct sifter_prog {
-    bool is_entry;
+    int type;
     std::string event;
     std::string entry;
     unique_fd fd;
-    sifter_prog(bool is_e, std::string ev, std::string en, int f):
-        is_entry(is_e), event(ev), entry(en), fd(android::base::unique_fd(f)) {};
+    sifter_prog(int t, std::string ev, std::string en, int f):
+        type(t), event(ev), entry(en), fd(android::base::unique_fd(f)) {};
 };
 
 class sifter_tracer {
+private:
     int m_init;
+    int m_verbose;
     std::string m_name;
     std::vector<sifter_prog> m_progs;
     std::vector<sifter_map> m_maps;
+    std::vector<sifter_rb> m_rbs;
+    std::vector<std::thread *> m_rbs_update_threads;
+    std::set<int> m_ignored_pids;
+    bool m_rbs_update_start;
+
+    void update_rbs_thread() {
+        m_ignored_pids.insert(gettid());
+        while (m_rbs_update_start) {
+            for (auto &rb : m_rbs) {
+                for (int p = 0; p < 32768; p++) {
+                    if (m_ignored_pids.find(p) != m_ignored_pids.end())
+                        continue;
+
+                    uint8_t ctr;
+                    android::bpf::findMapEntry(rb.ctr_fd, &p, &ctr);
+                    uint8_t last_ctr = rb.saved[p].ctr;
+                    if (ctr != last_ctr) {
+                        bool missing_events = (ctr != last_ctr+1);
+                        if (m_verbose > 0) {
+                            if (missing_events)
+                                std::cout << "Update events pid[" << p << "] "
+                                    << (int)ctr << " - " << (int)last_ctr << "\n";
+                            else
+                                std::cout << "Update events pid[" << p << "] "
+                                    << (int)ctr <<"\n";
+                        }
+                        rb_elem rbp;
+                        android::bpf::findMapEntry(rb.fd, &p, &rbp);
+                        rb.saved[p].ctr = ctr;
+                        rb.update_tbl(p, ctr, rbp, missing_events);
+                    }
+                }
+            }
+        }
+    }
 
 public:
-    int add_prog(bool is_entry, std::string event, std::string entry) {
-        std::string probe = is_entry? "_kprobe_" : "_kretporbe_";
-        std::string path = "/sys/fs/bpf/prog_" + m_name + probe + entry;
+    size_t map_num() {
+        return m_maps.size();
+    }
+
+    size_t rb_num() {
+        return m_rbs.size();
+    }
+
+    int add_prog(int type, std::string event, std::string entry) {
+        std::string probe_name;// = is_entry? "_kprobe_" : "_kretporbe_";
+        switch (type) {
+            case 0: probe_name = std::string("_kretprobe_") + entry; break;
+            case 1: probe_name = std::string("_kprobe_") + entry; break;
+            case 2: probe_name = std::string("_tracepoint_") + event + "_" + entry; break;
+        }
+        std::string path = "/sys/fs/bpf/prog_" + m_name + probe_name;
         int fd = bpf_obj_get(path.c_str());
         if (fd != -1)
-            m_progs.push_back(sifter_prog(is_entry, event, entry, fd));
+            m_progs.push_back(sifter_prog(type, event, entry, fd));
         return fd;
-    };
+    }
 
     int add_map(int type, std::string map) {
         std::string path = "/sys/fs/bpf/map_" + m_name + "_" + map;
@@ -53,19 +157,54 @@ public:
         if (fd != -1)
             m_maps.push_back(sifter_map(type, map, 2, fd));
         return fd;
-    };
+    }
+
+    int add_rb(int len, std::string map) {
+        std::string path = "/sys/fs/bpf/map_" + m_name + "_" + map;
+        int fd = bpf_obj_get(path.c_str());
+        path += "_ctr";
+        int ctr_fd = bpf_obj_get(path.c_str());
+        if (fd != -1 && ctr_fd != -1) {
+            m_rbs.push_back(sifter_rb(len, map, fd, ctr_fd));
+            return fd;
+        }
+        return -1;
+    }
 
     int attach_prog() {
         for (auto &p : m_progs) {
-            bpf_probe_attach_type type = p.is_entry? BPF_PROBE_ENTRY : BPF_PROBE_RETURN;
-            int ret = bpf_attach_kprobe(p.fd, type, p.event.c_str(), p.entry.c_str(), 0);
-            if (ret < 0) {
-                std::cout << "bpf_attach_kprobe return " << ret << " " << errno << "\n";
-                return -1;
+            if (p.type == 0 || p.type == 1) {
+                bpf_probe_attach_type type = p.type == 1? BPF_PROBE_ENTRY : BPF_PROBE_RETURN;
+                int ret = bpf_attach_kprobe(p.fd, type, p.event.c_str(), p.entry.c_str(), 0);
+                if (ret < 0) {
+                    std::cout << "bpf_attach_kprobe return " << ret << " " << errno << "\n";
+                    return -1;
+                }
+            } else if (p.type == 2) {
+                int ret = bpf_attach_tracepoint(p.fd, p.event.c_str(), p.entry.c_str());
+                if (ret < 0) {
+                    std::cout << "bpf_attach_tracepoint return " << ret << " " << errno << "\n";
+                    return -1;
+                }
             }
         }
         return 0;
-    };
+    }
+
+    void print_rbs() {
+        for (auto &rb : m_rbs) {
+            for (auto &entry : rb.tbl) {
+                for (auto it : entry.first)
+                    std::cout << std::setw(4) << it << " ";
+                std::cout << "| ";
+                for (int i = 0; i < 65536; i++)
+                    if (entry.second[i])
+                        std::cout << std::setw(4) << i << " ";
+                std::cout << "\n";
+            }
+            std::cout << "Total: " << rb.tbl.size() << " sequences" << "\n";
+        }
+    }
 
     void print_maps() {
         for (auto &m : m_maps) {
@@ -79,7 +218,7 @@ public:
             }
             std::cout << "]\n";
         }
-    };
+    }
 
     void dump_maps(std::string file) {
         std::ofstream ofs(file.c_str());
@@ -88,7 +227,7 @@ public:
                 ofs << v << " ";
             ofs << "\n";
         }
-    };
+    }
 
     void recover_maps(std::string file) {
         std::ifstream ifs(file.c_str());
@@ -107,16 +246,16 @@ public:
                 }
             }
         } else {
-            for (auto &m: m_maps) {
+            for (auto &m : m_maps) {
                 for (auto &v : m.val)
                     ifs >> v;
             }
         }
 
-    };
+    }
 
     void update_maps() {
-        for (auto &m: m_maps) {
+        for (auto &m : m_maps) {
             int size = m.val.size();
             std::vector<uint64_t> val(size);
             for (int i = 0; i < size; i++)
@@ -133,15 +272,29 @@ public:
                     break;
             }
         }
-    };
+    }
+
+    void start_update_rbs() {
+        m_ignored_pids.insert(gettid());
+        m_rbs_update_start = 1;
+        std::thread *th = new std::thread(&sifter_tracer::update_rbs_thread, this);
+        m_rbs_update_threads.push_back(th);
+    }
+
+    void stop_update_rbs() {
+        m_rbs_update_start = 0;
+        for (auto &th : m_rbs_update_threads) {
+            th->join();
+        }
+    }
 
     operator bool() const {
         return m_init == 1;
-    };
+    }
 
     sifter_tracer(): m_init(0) {};
 
-    sifter_tracer(std::string file): m_init(0) {
+    sifter_tracer(std::string file, int verbose=0): m_init(0), m_verbose(verbose) {
         std::ifstream ifs(file);
 
         if (!ifs) {
@@ -155,17 +308,28 @@ public:
         while (ifs >> cfg_type) {
             switch (cfg_type) {
                 case 'p': {
-                    bool is_entry;
+                    int type;
                     std::string event, entry;
-                    ifs >> is_entry >> event >> entry;
-                    add_prog(is_entry, event, entry);
+                    ifs >> type >> event >> entry;
+                    if (add_prog(type, event, entry) == -1) {
+                        return;
+                    }
                     break;
                 }
                 case 'm': {
                     int type;
                     std::string name;
                     ifs >> type >> name;
-                    add_map(type, name);
+                    if (add_map(type, name) == -1)
+                        return;
+                    break;
+                }
+                case 'r': {
+                    int length;
+                    std::string name;
+                    ifs >> length >> name;
+                    if (add_rb(length, name) == -1)
+                        return;
                     break;
                 }
                 default:
@@ -176,38 +340,53 @@ public:
         }
 
         m_init = 1;
-    };
+    }
+
 };
 
 int main(int argc, char *argv[]) {
     bool manual_mode = false;
     int log_interval = 10;
+    int verbose = 0;
     char *config_file = nullptr;
     char *log_file = nullptr;
 
     int opt;
-    while ((opt = getopt (argc, argv, "mi:c:o:")) != -1) {
+    while ((opt = getopt (argc, argv, "hmi:v:c:o:")) != -1) {
         switch (opt) {
+            case 'h':
+                std::cout << "Sifter agent\n";
+                std::cout << "Options\n";
+                std::cout << "-c config   : agent configuration file [required]\n";
+                std::cout << "-m          : read maps\n";
+                std::cout << "-i interval : maps logging interval in seconds [default=10]\n";
+                std::cout << "-o output   : maps logging output file\n";
+                std::cout << "-v verbose  : \n";
+                std::cout << "-h          : helps\n";
+                return 0;
             case 'm': manual_mode = true; break;
             case 'i': log_interval = atoi(optarg); break;
+            case 'v': verbose = atoi(optarg); break;
             case 'c': config_file = optarg; break;
             case 'o': log_file = optarg; break;
             case '?':
                 if (optopt == 'c')
-                    fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+                    fprintf(stderr, "Option -%c requires an argument.\n", optopt);
                 else if (isprint (optopt))
-                    fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+                    fprintf(stderr, "Unknown option `-%c'.\n", optopt);
                 else
-                    fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
+                    fprintf(stderr, "Unknown option character `\\x%x'.\n", optopt);
                 return 1;
             default: abort();
         }
     }
 
-    sifter_tracer tracer(config_file);
+    sifter_tracer tracer(config_file, verbose);
 
-    if (!tracer)
+    if (!tracer) {
+        std::cout << "Failed to create tracer\n";
         return 1;
+    }
 
     tracer.attach_prog();
 
@@ -218,13 +397,24 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
-    tracer.recover_maps(log_file);
-    std::string tmp_file = std::string(log_file) + ".tmp";
-    while (1) {
-        tracer.update_maps();
-        tracer.dump_maps(tmp_file);
-        std::rename(tmp_file.c_str(), log_file);
-        sleep(log_interval);
+    if (tracer.rb_num() > 0) {
+        tracer.start_update_rbs();
+        std::cout << "\nPress enter to read seq values...\n";
+        std::cin.get();
+        tracer.print_rbs();
+        tracer.stop_update_rbs();
+        return 0;
+    }
+
+    if (tracer.map_num() > 0) {
+        tracer.recover_maps(log_file);
+        std::string tmp_file = std::string(log_file) + ".tmp";
+        while (1) {
+            tracer.update_maps();
+            tracer.dump_maps(tmp_file);
+            std::rename(tmp_file.c_str(), log_file);
+            sleep(log_interval);
+        }
     }
 
 }
